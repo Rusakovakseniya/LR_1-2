38a1932eb07a8e8d88c977c73b1aeb391e21ed61
Rusakovakseniya <144375444+Rusakovakseniya@users.noreply.github.com>
2023-10-26 14:25:32 +0300

    Add files via upload

---
+++ блок 3.1 лр 1-2.pas
@@ -0,0 +1,9 @@
+﻿program zad1;
+var a,b,c,min:integer;
+begin
+readln(a,b,c);
+min:=a;
+if b<min then b:=min;
+if c<min then c:=min;
+writeln(min);
+end.
---
+++ блок 3.4 лр 1-2.pas
@@ -0,0 +1,12 @@
+﻿program zad4;
+var a, b, c, D, x1, x2: real;
+begin
+write('a = ');read(a);
+write('b = ');read(b);
+write('c = ');read(c);
+D:=b*b-4*a*c;
+x1:=(-b + sqrt(D))/(2*a);
+ x2:=(-b - sqrt(D))/(2*a);
+writeln('x1 = ', x1);
+writeln('x2 = ', x2);
+end.
